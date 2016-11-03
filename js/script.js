fd547d3462da91386c59fc97f0cd091b5e51aac7
Stephanie Su <stephanienewegg@gmail.com>
2016-11-03 10:07:14 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 
-//PopUp示意
 $(function (){
     $(".btn[pop]").on("click",function(){
         $(".pop_details").css({opacity:1}).fadeIn();
@@ -12,5 +11,43 @@ $(function (){
         $("html").css("overflow","");
     });
 
-    $(".turnOn").hide();
+    $(".navbar_toggle_line").addClass("turnOn"); $(".turnOn").hide();
 });
+
+$(function(){
+    $(window).scroll(function(){
+        var $bodyTop=$("body").scrollTop();
+        var nav = $(".nav_wrapper");
+        if($bodyTop>85) {
+            nav.addClass("fixed");
+        }
+        else {
+            nav.removeClass("fixed");
+        }
+    });
+});
+
+
+$(function(){
+
+    var $body=$("body");
+
+    $(".stephanie").css("cursor","pointer").on("click",function(){
+        $body.animate({
+            scrollTop:0
+        },500);
+    });
+    $(".pageTo").each(function(){
+        $(this).on("click",function(){
+            var className = $(this).attr("class").split(' ')[1];
+            var targetTop = $("section."+className).offset().top;
+            $body.animate({
+                scrollTop: targetTop
+            },1000);
+        });
+    });
+    
+
+});
+
+
